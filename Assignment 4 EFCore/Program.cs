224a2250ad95792bac8e4cf2b26b5d91035dd6ed
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-09-29 16:09:03 +0300

    Add project files.

---
+++ Assignment 4 EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_4_EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
